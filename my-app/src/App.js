62350817d4be2eb0476c44545280bc9d23dfd006
temka <xerox4031@gmail.com>
2023-04-03 23:30:54 +0800

    fhdaskfa

---
+++ my-app/src/App.js
@@ -0,0 +1,28 @@
+// props, module.css
+//  engiin function gaas arai baga bicigelltei gj uzne ghde
+//  uildel zarim ng zuilese hyzgaarlaldmal
+
+
+import React from "react"; 
+import ArrowFunction from "./components/props";
+import zurag1 from "./assets/image/14.jpeg"
+import "./App.module.css"
+
+// props, module.css
+var getM = new Date().getMonth();
+var getD = new Date().getDate();
+var getA = new Date().getHours();
+var toCompile = getM.toString()
+var toCompile2 = getD.toString()
+var toCompile3 = getA.toString()
+function App() {
+  return (
+     <div>
+        <ArrowFunction date={"0" + toCompile + "/" + "0" + toCompile2 + "/" + toCompile3} image={zurag1} tittle="ene propsooos damjeelaggdaj baigaa utga damjuulalt" des="ene bol des propsoos damjigdaj baigaa utga" />
+        <ArrowFunction date={toCompile} tittle="gffgdfgdfsgdfsgdfs" />
+        <ArrowFunction tittle="ene bol 3 dahi components" />
+        
+     </div>
+  );
+}
+export default App;
---
+++ my-app/src/components/props/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import zurag from "../../assets/image/14.jpeg"
+import styles from './style.module.css'
+// arrow function
+// engiin function gaas arai baga bichigleltei gj uzne gehdee
+// uildel ni zarim neg zuileese hyzgaarlagdmal 
+
+
+const ArrowFunction = (props) => {
+    // block code
+    return(
+        <>
+            <div className={styles.box}>
+                <div>
+                    <img src={props.image} />
+                </div>
+                <div>
+                    <h1>{props.tittle}</h1>
+                    <h1>{props.des}</h1>
+                    <p>{props.date}</p>
+                </div>
+                {/* <img className={styles.image} src={props.image}/>
+                <h1 className={styles.garchig}>{props.tittle}</h1>
+                <p className={styles.text}>{props.date}</p> */}
+            </div>
+        </>
+    )
+}
+export default ArrowFunction
